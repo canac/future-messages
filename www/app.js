5e947c17c9becbba9c6a4321b669e50f667b8240
Caleb Cox <canac@users.noreply.github.com>
2019-01-06 23:20:25 -0500

    Add message creation

--- www/app.js
+++ www/app.js
@@ -1,12 +1,35 @@
 'use strict';
 
+function normalizeMessage (message) {
+  message.notifyTime = new Date(message.notifyTime);
+}
+
 async function loadMessages () {
   const res = await fetch(`/api/messages`);
   const messages = await res.json();
-  messages.forEach(message => {
-    message.notifyTime = new Date(message.notifyTime);
+  messages.forEach(normalizeMessage);
+  return messages;
+}
+
+async function createMessage (content) {
+  // Set notify time to tomorrow at noon
+  const notifyTime = new Date();
+  notifyTime.setMilliseconds(0);
+  notifyTime.setSeconds(0);
+  notifyTime.setMinutes(0);
+  notifyTime.setHours(12);
+  notifyTime.setTime(notifyTime.getTime() + 24 * 60 * 60 * 1000);
+
+  const res = await fetch(`/api/messages`, {
+    method: `post`,
+    headers: {
+      'Content-Type': `application/json`,
+    },
+    body: JSON.stringify({ content, notifyTime }),
   });
-  return messages;  
+  const message = await res.json();
+  normalizeMessage(message);
+  return message;
 }
 
 document.addEventListener(`DOMContentLoaded`, async () => {
@@ -14,6 +37,15 @@ document.addEventListener(`DOMContentLoaded`, async () => {
     el: `#app`,
     data: {
       messages: [],
+      newMessageContent: ``,
+    },
+    methods: {
+      createMessage() {
+        createMessage(app.newMessageContent).then(message => {
+          app.messages.push(message);
+          app.newMessageContent = ``;
+        });
+      },
     },
   });
 
